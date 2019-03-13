7d1ad2ecae61ee029e282fd9c06a68a01634c030
basilije <basilije@users.noreply.github.com>
2019-03-13 14:32:15 -0700

    Update Main.cpp

--- Exercise2/Main.cpp
+++ Exercise2/Main.cpp
@@ -10,9 +10,8 @@ int main()
 	for (unsigned int i = 1; i <= 1000000; i++)
 		one_million_of_ints.push_back(i);
 
-	vector<int> one_million_of_ints_evenly_divisible_by_7(one_million_of_ints.size());
-	int count7 = distance(one_million_of_ints_evenly_divisible_by_7.begin(), copy_if(one_million_of_ints.begin(), one_million_of_ints.end(), one_million_of_ints_evenly_divisible_by_7.begin(), [](int i) {return (i % 7 == 0); }));
-	cout << "Numbers in the vector that are evenly divisible by 7: " << count7 << endl;
+	int count7 = count_if(one_million_of_ints.begin(), one_million_of_ints.end(), [](int i) {return (i % 7 == 0); });
+	cout << "Number of the numbers in the vector that are evenly divisible by 7: " << count7 << endl;
 
 	return 0;
 }
